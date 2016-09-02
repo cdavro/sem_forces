dab2d0b9db33a4dd6e82c4654311b340f2de8c43
Rolf David <rolf.david23@gmail.com>
2016-09-02 17:37:52 +0200

    Rewrite, using LAPACK/MKL

--- sem_forces.f90
+++ sem_forces.f90
@@ -1,33 +1,34 @@
 program matrix
-implicit none
+ implicit none
+ integer :: N=3 
+ integer :: LDA=3,LDVL=3,LDVR=3
+ integer,parameter :: LWMAX=1000
+ integer :: INFO, LWORK 
+ real*8,dimension(3,3) :: AM, VR, VL
+ real*8,dimension(3) ::  WR, WI
+ real*8,allocatable :: WORK (:)
  
  real,allocatable :: atmat(:,:), atmcrd (:,:)
- 
  integer,allocatable :: atmlnb (:), atmlan (:)
- 
- real,dimension(3,3) :: satmat, egvec
- real,dimension(3) ::  egval
-
  character(len=2),allocatable :: atmlna (:) 
- character(len=8),dimension(:),allocatable :: atmlnbc (:)
-
-  
- integer :: o = 3, it_max=1000, it_num, rot_num
- real :: error_frobenius
-
+ real,dimension(3) :: dvector
+ real :: dist
  real :: xvector, yvector, zvector
  integer :: atm1, atm2, smx, smy, nbatm, matsize
  real :: k1, k2, k3, kf
 
  character :: revar
- integer :: i, j, n = 1
-  
+ integer :: i, j
+
+ external DGEEV
+ 
  write (*,"(a)") "Please input the number of atoms ?"
  read(*,*) nbatm
-
- matsize=nbatm*3
+ write (*,"(a)") ""
  
- open(unit=50,file="fchk",status='old', access='sequential', form='formatted', action='read')
+ matsize=nbatm*3
+!READING FILE 
+  open(unit=50,file="data",status='old', access='sequential', form='formatted', action='read')
   allocate(atmcrd(5,nbatm))
   allocate(atmlnb(matsize))
   allocate(atmlan(matsize))
@@ -42,18 +43,6 @@ implicit none
   read(50,*) revar ! advance one line
     read(50,*) atmat
  close(50)
-! allocate(atmlnbc(matsize))
-! do i=1,matsize
-! print*, i
-! print*, atmlnb(i)
-! print*, atmlnbc(i)
-! write(atmlnbc(i),*) atmlnb(i) 
-! print*, "hola"
-! atmlnbc(i)=atmlnb(i)
-! write(atmlnbc,*) atmlnb
-
-! end do
-! print*, "hola"
  write (*,"(a)") "", "This is the list of atoms:"
  do i=1,nbatm
    write (*,"(a,I0,a)" ,advance="NO") trim(atmlna(i*3-2)), atmlnb((i*3-2)), " "
@@ -66,62 +55,73 @@ implicit none
  smy=((atm2*3)-3)
  do i=1,3
   do j=1,3
-   satmat(i,j) = atmat((smx+i),(smy+j))
+   AM(i,j) = atmat((smx+i),(smy+j))
   end do
  end do
-
- satmat=-1*transpose(satmat)
- print*, atmlnb 
+!END READ FILE
+!PRINT FORCE MATRIX
+ AM=-1*transpose(AM)
  write (*,"(a)") ""
- write (*,"(7a)") "-1 * Cartesian Force Matrix (kcal/mol) between atom ", atmlna(atm1*3-2), atmlnb(atm1*3-2), &
-  " and atom ", atmlna(atm2*3-2), atmlnb(atm2*3-2), " :"
+write (*,"(2a,I0,2a,I0,a)") "Interatomic Cartesian Force Matrix (a.u.) between atom ", trim(atmlna(atm1*3-2)), atmlnb(atm1*3-2), &
+  " and atom ", trim(atmlna(atm2*3-2)), atmlnb(atm2*3-2), " :"
  write (*,"(a)") ""
  do i=1,3
   do j=1,3
-    write(*, "(E14.6E2,a)" , advance="no") satmat(i,j), ""
+    write(*, "(E14.6E2,a)" , advance="no") (AM(i,j)), ""
   end do
   write (*,"(a)") ""
  end do
-
- call jacobi_eigenvalue ( 3, satmat, it_max, egvec, egval, it_num, rot_num )
-    write ( *, '(a,i4)' ) '  Number of iterations = ', it_num
-  write ( *, '(a,i4)' ) '  Number of rotations  = ', rot_num
-
- write(*,"(a)") "", "Eigen Values (kcal/mol):", ""
+ !END PRINT FORCE MATRIX
+!CALL FOR INTEL MKL 
+ LWORK = -1
+ allocate(WORK(LWORK))
+ call DGEEV ('V', 'V', N, AM, LDA, WR, WI, VL, LDVL, VR, LDVR, WORK, LWORK, INFO)
+ LWORK = MIN (LWMAX, INT(WORK(1)))
+ deallocate(WORK)
+ allocate(WORK(LWORK)) 
+ call DGEEV ('V', 'V', N, AM, LDA, WR, WI, VL, LDVL, VR, LDVR, WORK, LWORK, INFO)
+ IF( INFO.GT.0 ) THEN
+  WRITE(*,*)'The algorithm failed to compute eigenvalues.'
+   STOP
+  END IF
+ deallocate(WORK)
+!END CALL FOR INTEL MKL
+ write(*,"(a)") ""
+ write(*,"(a)") "", "Eigen Values (real) (a.u.):", ""
  do i=1,3
-  write(*, "(E14.5E2,a)", advance="no") egval(i), ""
+  write(*, "(E14.5E2,a)", advance="no") (WR(i)), ""
+ end do
+ write(*,"(a)") ""
+ write(*,"(a)") "", "Eigen Values (comp) (a.u.):", ""
+ do i=1,3
+  write(*, "(E14.5E2,a)", advance="no") (WI(i)), ""
  end do
  write (*,"(a)") "", "", "Eigen Vectors:", ""
  do i=1,3
   do j=1,3
    write(*, "(E14.6E2,a)" , advance="no") &
-     egvec(i,j), &
+     VR(i,j), &
      "  "
   end do
   write (*,"(a)") ""  
  end do
-
-call r8mat_is_eigen_right ( o, o, satmat, egvec, egval, error_frobenius )
-  write ( *, '(a)' ) ""
-  write ( *, '(a,E14.6E2)' ) 'Frobenius norm error in eigensystem A*V-D*V =', error_frobenius 
-
- atmcrd = transpose(atmcrd)
- xvector = atmcrd(atm2,3) - atmcrd(atm1,3)
- yvector = atmcrd(atm2,4) - atmcrd(atm1,4)
- zvector = atmcrd(atm2,5) - atmcrd(atm1,5)
+ xvector = (atmcrd(3,atm2) - atmcrd(3,atm1))
+ yvector = (atmcrd(4,atm2) - atmcrd(4,atm1))
+ zvector = (atmcrd(5,atm2) - atmcrd(5,atm1))
+ dist = SQRT(abs(xvector*2)+abs(yvector**2)+abs(zvector**2)) 
+ dvector(1) = xvector
+ dvector(2) = yvector
+ dvector(3) = zvector
  write (*,"(a)") ""
  write (*,"(a)") ""
- write (*,"(E14.6E2)" ,advance="NO") xvector
- write (*,"(E14.6E2)" ,advance="NO") yvector
- write (*,"(E14.6E2)") zvector
- k1 = egval(1) * abs( (egvec(1,1) * xvector) + (egvec(1,2) * yvector) + (egvec(1,3) * zvector)) 
- k2 = egval(2) * abs( (egvec(2,1) * xvector) + (egvec(2,2) * yvector) + (egvec(2,3) * zvector)) 
- k3 = egval(3) * abs( (egvec(3,1) * xvector) + (egvec(3,2) * yvector) + (egvec(3,3) * zvector)) 
-
+ write(*,"(a,F6.2,a)", advance='NO') "Distance (Å): ", dist*0.529177249, ""
+ write (*,"(a)") ""
+ k1=WR(1) * abs(dot_product(dvector,VR(:,1)))
+ k2=WR(2) * abs(dot_product(dvector,VR(:,2)))
+ k3=WR(3) * abs(dot_product(dvector,VR(:,3)))
  kf=(k1+k2+k3)
- write(*,"(E14.6E2)") kf
-! write (*,"(a)") ""
-! write (*,"(5a,F10.4,a)") "Approximate force constant between atom ", trim(atl(atm1)), " and atom ", trim(atl(atm2)), &
-! " : ", kf*2240.874995, " kcal/mol"
+ write (*,"(a)") ""
+ write (*,"(2a,I0,2a,I0,a,F10.2,a)") "Approximate force constant between atom " , trim(atmlna(atm1*3-2)), atmlnb(atm1*3-2), &
+       " and atom ", trim(atmlna(atm2*3-2)), atmlnb(atm2*3-2), " : ", kf, " a.u."
+ write (*,"(a)") ""
 end program matrix
-
